4e4d7b8b0b71ef48ce5e320a22d201fd1099fd04
bill <jojowil@gmail.com>
2025-06-19 17:53:41 -0400

    Initial Commit - Copied code from Replit.

---
+++ src/BlackJack.java
@@ -0,0 +1,156 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BlackJack {
+    static Scanner kb = new Scanner(System.in);
+
+    public enum Suit {Spades, Hearts, Clubs, Diamonds};
+
+    public enum Rank {
+        Two(2), Three(3), Four(4), Five(5), Six(6), Seven(7), Eight(8),
+        Nine(9), Ten(10), Jack(10), Queen(10), King(10), Ace(11);
+
+        private final int value;
+
+        private Rank (int v) {
+            value = v;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private static record Card (Rank r, Suit s) {
+    /*
+    Suit s;
+    Rank r;
+
+    public Card (Rank r, Suit s) {
+      this.s = s;
+      this.r = r;
+    }
+    */
+
+        @Override
+        public String toString() {
+            return r + " of " + s;
+        }
+
+        public int getValue() {
+            return r.getValue();
+        }
+    }
+
+    public static void main(String[] args) {
+        char choice;
+        LinkedList<Card> deck = new LinkedList<>();
+        ArrayList<Card> dealer = new ArrayList<>();
+        ArrayList<Card> player = new ArrayList<>();
+
+        // populate the deck
+        for (Suit suit : Suit.values())
+            for (Rank rank : Rank.values())
+                deck.add(new Card(rank, suit));
+
+        // how many cards?
+        System.out.println("There are " + deck.size() + " cards in the deck.");
+
+        // displaying unshuffled deck.
+        //showHand(deck);
+
+        Collections.shuffle(deck);
+        // displaying shuffled deck.
+        //showHand("The deck", deck);
+
+        deal(deck, dealer, player);
+
+        showHand("Dealer", dealer);
+        System.out.println(handValue(dealer));
+        showHand("Player", player);
+        System.out.println(handValue(player));
+        int dhv = handValue(dealer);
+        int phv = handValue(player);
+        if ( dhv != 21 ) {
+            do {
+                System.out.print("(H)it or (S)tay? ");
+                choice = kb.next().toUpperCase().charAt(0);
+                if ( choice == 'H' ) {
+                    player.add(deck.pop());
+                    showHand("Player", player);
+                    phv = handValue(player);
+                    System.out.println(handValue(player));
+                }
+            } while (choice != 'S' && phv < 22 && player.size() < 5);
+
+            //dhv = handValue(dealer);
+            phv = handValue(player);
+
+            if ( player.size() < 5 )
+                while (dhv < phv && (phv <= 21)) {
+                    dealer.add(deck.pop());
+                    dhv = handValue(dealer);
+                }
+        }
+
+        if ( dhv == 21 && dealer.size() == 2)
+            System.out.println("Dealer has BlackJack!");
+        else if ( player.size() == 5 && phv < 22 )
+            System.out.println("You won with a 5-card Charlie! Woohoo!");
+        else if (dhv > 21)
+            System.out.println("Dealer has bust! You Win!");
+        else if (phv > 21)
+            System.out.println("Player has bust! Dealer Wins!");
+        else if ( dhv >= phv )
+            System.out.println("Dealer wins!");
+        else
+            System.out.println("You win!");
+        showHand("Dealer", dealer);
+        showHand("Player", player);
+        System.out.println("Dealer has " + dhv + ". Player has " + phv);
+    }
+
+    private static void deal (LinkedList<Card> deck, ArrayList<Card> d, ArrayList<Card> p) {
+        for ( int x = 0; x < 2; x++) {
+            p.add(deck.pop());
+            d.add(deck.pop());
+        }
+    }
+
+    private static int handValue(ArrayList<Card> hand) {
+        int total = 0;
+        //Object[] ca = hand.toArray();
+        int[] ia = new int[hand.size()];
+
+        int x = 0;
+        for (Card c : hand) {
+            int cv = c.getValue();
+            ia[x] = cv;
+            x++;
+        }
+
+        Arrays.sort(ia);
+        for (int i : ia) {
+            //  System.out.println(i);
+
+            if ( i == 11 && (total + 11 > 21) )
+                i = 1;
+
+            total = total + i;
+        }
+
+        return total;
+    }
+
+    private static void showHand(String name, List<Card> hand) {
+        System.out.println("========================================");
+        System.out.println(name + " has:");
+        for (Card c : hand)
+            System.out.println(c);
+    }
+
+}
---
+++ src/FiveCardStud.java
@@ -0,0 +1,211 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+//import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class FiveCardStud {
+    static Scanner kb = new Scanner(System.in);
+
+    public enum Suit {Spades, Hearts, Clubs, Diamonds};
+
+    public enum Rank {Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace};
+
+    private static class CardComparator implements Comparator<Card> {
+
+        @Override
+        public int compare(Card c1, Card c2) {
+            return c1.r.compareTo(c2.r);
+        }
+    }
+
+    private static class Card {
+        Suit s;
+        Rank r;
+
+        public Card (Rank r, Suit s) {
+            this.s = s;
+            this.r = r;
+        }
+
+        @Override
+        public String toString() {
+            return r + " of " + s;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Card> deck = new LinkedList<Card>();
+        ArrayList<Card> dealer = new ArrayList<Card>();
+        ArrayList<Card> player = new ArrayList<Card>();
+
+        // populate the deck
+        for (Suit suit : Suit.values())
+            for (Rank rank : Rank.values())
+                deck.add(new Card(rank, suit));
+
+        // how many cards?
+        System.out.println("There are " + deck.size() + " cards in the deck.");
+
+        // displaying unshuffled deck.
+        //showHand(deck);
+
+        Collections.shuffle(deck);
+        // displaying shuffled deck.
+        //showHand("The deck", deck);
+
+        deal(deck, dealer, player);
+
+        CardComparator cc = new CardComparator();
+
+        dealer.sort(cc);
+        showHand("Dealer", dealer);
+        player.sort(cc);
+        showHand("Player", player);
+
+        System.out.print("Dealer has ");
+        handRanking(dealer);
+        System.out.print("Player has ");
+        handRanking(player);
+
+    }
+
+    private static void handRanking(ArrayList<Card> h) {
+
+        if (isRoyalFlush(h) )
+            System.out.println("Royal Flush");
+        else if (isStraightFlush(h))
+            System.out.println("Straight Flush");
+        else if (is4OfAKind(h))
+            System.out.println("4 of a kind");
+        else if (isFullHouse(h))
+            System.out.println("Full House");
+        else if (isFlush(h))
+            System.out.println("Flush");
+        else if (isStraight(h))
+            System.out.println("Straight");
+        else if (is3OfAKind(h))
+            System.out.println("3 of a kind");
+        else if (isTwoPair(h))
+            System.out.println("Two Pair");
+        else if (isPair(h))
+            System.out.println("Pair");
+        else
+            System.out.println(h.get(4) + " high");
+    }
+
+    private static void deal (LinkedList<Card> deck, ArrayList<Card> d, ArrayList<Card> p) {
+        for ( int x = 0; x < 5; x++) {
+            p.add(deck.pop());
+            d.add(deck.pop());
+        }
+    }
+
+    private static void showHand(String name, List<Card> hand) {
+        System.out.println("========================================");
+        System.out.println(name + " has:");
+        for (Card c : hand)
+            System.out.println(c);
+    }
+
+    private static boolean isFlush(ArrayList<Card> h) {
+        Suit s = h.get(0).s;
+
+        for (int x = 1; x < 5; x++)
+            if (h.get(x).s != s)
+                return false;
+
+        return true;
+    }
+
+    private static boolean isStraight(ArrayList<Card> h) {
+        int r = h.get(0).r.ordinal();
+
+        for (int x = 1; x < 5; x++)
+            if (h.get(x).r.ordinal() != r+x)
+                return false;
+
+        return true;
+    }
+
+    private static boolean isStraightFlush(ArrayList<Card> h) {
+
+        return isFlush(h) && isStraight(h);
+    }
+
+    private static boolean isRoyalFlush(ArrayList<Card> h) {
+
+        return isStraightFlush(h) && h.get(0).r == Rank.Ten;
+    }
+
+    public static boolean is4OfAKind(ArrayList<Card> h) {
+        Rank r1 = h.get(0).r;
+        Rank r2 = h.get(1).r;
+        Rank r3 = h.get(2).r;
+        Rank r4 = h.get(3).r;
+        Rank r5 = h.get(4).r;
+
+        if ( (r1 == r2 && r2 == r3 && r3 == r4) || (r2 == r3 && r3 == r4 && r4 == r5) )
+            return true;
+
+        return false;
+    }
+
+    public static boolean isFullHouse(ArrayList<Card> h) {
+        Rank r1 = h.get(0).r;
+        Rank r2 = h.get(1).r;
+        Rank r3 = h.get(2).r;
+        Rank r4 = h.get(3).r;
+        Rank r5 = h.get(4).r;
+
+        if ( ((r1 == r2) &&  (r3 == r4 && r4 == r5)) || ((r1 == r2 && r2 == r3) && (r4 == r5)) )
+            return true;
+
+        return false;
+    }
+
+    public static boolean is3OfAKind(ArrayList<Card> h) {
+        Rank r1 = h.get(0).r;
+        Rank r2 = h.get(1).r;
+        Rank r3 = h.get(2).r;
+        Rank r4 = h.get(3).r;
+        Rank r5 = h.get(4).r;
+
+        if ( (r1 == r2 && r2 == r3) || (r2 == r3 && r3 == r4) || (r3 == r4 && r4 == r5) )
+            return true;
+
+        return false;
+    }
+
+    public static boolean isTwoPair(ArrayList<Card> h) {
+        Rank r1 = h.get(0).r;
+        Rank r2 = h.get(1).r;
+        Rank r3 = h.get(2).r;
+        Rank r4 = h.get(3).r;
+        Rank r5 = h.get(4).r;
+
+
+        if ( (r1 == r2) && ((r3 == r4) || (r4 == r5)) ||
+                ((r2 == r3) && (r4 == r5)) )
+            return true;
+
+        return false;
+    }
+
+    public static boolean isPair(ArrayList<Card> h) {
+        Rank r1 = h.get(0).r;
+        Rank r2 = h.get(1).r;
+        Rank r3 = h.get(2).r;
+        Rank r4 = h.get(3).r;
+        Rank r5 = h.get(4).r;
+
+
+        if ( (r1 == r2) || (r2 == r3) || (r3 == r4) || (r4 == r5) )
+            return true;
+
+        return false;
+    }
+}
