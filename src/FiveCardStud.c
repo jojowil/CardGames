462d0c6418054b6fc6bdefcecd442c10ae0906cb
bill <jojowil@gmail.com>
2025-11-22 19:12:01 -0500

    Refactored to a single source file and removed most warnings.

--- src/FiveCardStud.c
+++ src/FiveCardStud.c
@@ -2,7 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "FiveCardStud.h"
+#include "git-banned.h"
+
+// Suits enumeration.
+typedef enum {Spades, Hearts, Clubs, Diamonds, NumSuit} Suit;
+
+// Ranks enumeration.
+typedef enum {
+    Two, Three, Four, Five, Six, Seven, Eight,
+    Nine, Ten, Jack, Queen, King, Ace, NumRank
+} Rank;
+
+// Card definition.
+typedef struct {
+    Rank rank;
+    Suit suit;
+} Card;
+
+// A deck of cards.
+typedef struct {
+    Card cards[52];
+    int numCards;
+    int deckPos;
+} Deck;
+
+// A player's hand. (UNUSED)
+typedef struct {
+    Card cards[10];
+    int numCards;
+} Hand;
+
+const char* ranks[] = {
+    "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+    "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+};
+const char* suits[] = {"Spades", "Hearts", "Clubs", "Diamonds"};
+
+static const char *strRank(Rank r);
+static const char *strSuit(Suit s);
+static void populateDeck(Deck *d);
+static void shuffle(void *array, size_t n, size_t size);
+static void addCard(Deck *deck, Deck *hand);
+static void deal (Deck *deck, Deck *d, Deck *p);
+static int compareCards( const void *a, const void *b );
+static void showHand(char* name, Deck hand);
+
+static int isFlush(Deck h);
+static int isStraight(Deck h);
+static int isStraightFlush(Deck h);
+static int isRoyalFlush(Deck h);
+static int is4OfAKind(Deck h);
+static int isFullHouse(Deck h);
+static int is3OfAKind(Deck h);
+static int isTwoPair(Deck h);
+static int isPair(Deck h);
 
 // Convert rank to string.
 static const char *strRank(Rank r) {return ranks[r];}
